3ada1f4f13671f9d63f66892275e3e2f5db909c5
Anand <anandbaskaran3193@gmail.co>
2021-11-03 15:13:54 +0100

    Added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,32 +16,39 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted user and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
-    username = serializers.CharField(label=_("Username"))
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
     password = serializers.CharField(
-        label=_("Password"),
         style={'input_type': 'password'},
         trim_whitespace=False
     )
 
     def validate(self, attrs):
-        username = attrs.get('username')
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
         password = attrs.get('password')
 
-        if username and password:
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
-
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
-            if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
         return attrs
